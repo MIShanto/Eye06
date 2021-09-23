ec1fa5a62b5f4e333c7b5464b751a0d04fe52a66
MIShanto <shanto4045@gmail.com>
2021-09-23 12:52:42 +0600

    habijabi

--- sgp/sgp.py
+++ sgp/sgp.py
@@ -1,21 +1,33 @@
 from sgp4 import omm
 from sgp4.api import Satrec
 
+from skyfield.api import load, wgs84
 
-my_list = []
+stations_url = 'https://celestrak.com/NORAD/elements/1999-025.txt'
+satellites = load.tle_file(stations_url)
+print('Loaded', len(satellites), 'debris')
 
-
-with open('sample_omm.csv', encoding="utf-8") as f:
-        fields = next(omm.parse_csv(f))
-sat = Satrec()
-omm.initialize(sat, fields)
-
-# s = '1 25544U 98067A   19343.69339541  .00001764  00000-0  38792-4 0  9991'
-# t = '2 25544  51.6439 211.2001 0007417  17.6667  85.6398 15.50103472202482'
-
-jd, fr = 2458827, 0.362605
-e, r, v = sat.sgp4(jd, fr)
+by_name = {sat.name: sat for sat in satellites}
+#satellite = by_name['FENGYUN 1C DEB']
+print(by_name)
 
 
-print(r)  # True Equator Mean Equinox position (km)
-print(v)  # True Equator Mean Equinox velocity (km/s)
+
+
+
+
+
+
+
+from skyfield.api import EarthSatellite
+import datetime
+
+ts = load.timescale()
+t = ts.utc(2018, 7, 31)
+#t = ts.now()
+
+
+# line1 = '1 44383U 19006DE  21265.48736934  .00034058  10198-5  45670-3 0  9997'
+# line2 = '2 44383  96.1742 273.4086 0633428 125.2000 241.0218 14.50379804121473'
+# satellite = EarthSatellite(line1, line2, 'MICROSAT-R DEB', ts)
+# print(satellite.at(t).position.km)
