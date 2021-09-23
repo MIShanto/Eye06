794dee6a6e7ae5bc4f6f782a5663b283801cb460
Mehedi Hassan Rico <mhrico2@gmail.com>
2021-09-23 13:13:40 +0600

    Improve formatting

--- sgp/sgp.py
+++ sgp/sgp.py
@@ -8,4 +8,4 @@ print('Loaded', len(satellites), 'debris')
 ts = load.timescale()
 t = ts.now()
 for sat in satellites:
-    print(sat.at(t).position.km)
+    print(str(sat) + " " + str(sat.at(t).position.km))
